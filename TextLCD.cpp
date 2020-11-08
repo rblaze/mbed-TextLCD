9ed7e48f5e8852bc5ea886918c98296dace60ff4
Andrey Sverdlichenko <blaze@rusty.zone>
2020-11-08 16:37:51 -0500

    Format source code

--- TextLCD.cpp
+++ TextLCD.cpp
@@ -21,91 +21,93 @@
  */
 
 #include "TextLCD.h"
-#include "mbed.h"
 
-TextLCD::TextLCD(PinName rs, PinName e, PinName d4, PinName d5,
-                 PinName d6, PinName d7, LCDType type) : _rs(rs),
-        _e(e), _d(d4, d5, d6, d7),
-        _type(type) {
+#include <mbed.h>
 
-    _e  = 1;
-    _rs = 0;            // command mode
+TextLCD::TextLCD(
+    PinName rs, PinName e, PinName d4, PinName d5, PinName d6, PinName d7,
+    LCDType type)
+    : _rs(rs), _e(e), _d(d4, d5, d6, d7), _type(type) {
+  _e = 1;
+  _rs = 0;  // command mode
 
-    ThisThread::sleep_for(15ms);        // Wait 15ms to ensure powered up
+  ThisThread::sleep_for(15ms);  // Wait 15ms to ensure powered up
 
-    // send "Display Settings" 3 times (Only top nibble of 0x30 as we've got 4-bit bus)
-    for (int i=0; i<3; i++) {
-        writeByte(0x3);
-        ThisThread::sleep_for(2ms);  // this command takes 1.64ms, so wait for it
-    }
-    writeByte(0x2);     // 4-bit mode
-    wait_us(40);    // most instructions take 40us
+  // send "Display Settings" 3 times (Only top nibble of 0x30 as we've got 4-bit
+  // bus)
+  for (int i = 0; i < 3; i++) {
+    writeByte(0x3);
+    ThisThread::sleep_for(2ms);  // this command takes 1.64ms, so wait for it
+  }
+  writeByte(0x2);  // 4-bit mode
+  wait_us(40);     // most instructions take 40us
 
-    writeCommand(0x28); // Function set 001 BW N F - -
-    writeCommand(0x0C);
-    writeCommand(0x6);  // Cursor Direction and Display Shift : 0000 01 CD S (CD 0-left, 1-right S(hift) 0-no, 1-yes
-    cls();
+  writeCommand(0x28);  // Function set 001 BW N F - -
+  writeCommand(0x0C);
+  writeCommand(0x6);  // Cursor Direction and Display Shift : 0000 01 CD S (CD
+                      // 0-left, 1-right S(hift) 0-no, 1-yes
+  cls();
 }
 
 void TextLCD::character(int column, int row, int c) {
-    int a = address(column, row);
-    writeCommand(a);
-    writeData(c);
+  int a = address(column, row);
+  writeCommand(a);
+  writeData(c);
 }
 
 void TextLCD::cls() {
-    writeCommand(0x01); // cls, and set cursor to 0
-    ThisThread::sleep_for(2ms);     // This command takes 1.64 ms
-    locate(0, 0);
+  writeCommand(0x01);          // cls, and set cursor to 0
+  ThisThread::sleep_for(2ms);  // This command takes 1.64 ms
+  locate(0, 0);
 }
 
 void TextLCD::locate(int column, int row) {
-    _column = column;
-    _row = row;
+  _column = column;
+  _row = row;
 }
 
 int TextLCD::_putc(int value) {
-    if (value == '\n') {
-        _column = 0;
-        _row++;
-        if (_row >= rows()) {
-            _row = 0;
-        }
-    } else {
-        character(_column, _row, value);
-        _column++;
-        if (_column >= columns()) {
-            _column = 0;
-            _row++;
-            if (_row >= rows()) {
-                _row = 0;
-            }
-        }
+  if (value == '\n') {
+    _column = 0;
+    _row++;
+    if (_row >= rows()) {
+      _row = 0;
     }
-    return value;
+  } else {
+    character(_column, _row, value);
+    _column++;
+    if (_column >= columns()) {
+      _column = 0;
+      _row++;
+      if (_row >= rows()) {
+        _row = 0;
+      }
+    }
+  }
+  return value;
 }
 
 void TextLCD::writeByte(int value) {
-    _d = value >> 4;
-    wait_us(40); // most instructions take 40us
-    _e = 0;
-    wait_us(40);
-    _e = 1;
-    _d = value >> 0;
-    wait_us(40);
-    _e = 0;
-    wait_us(40);  // most instructions take 40us
-    _e = 1;
+  _d = value >> 4;
+  wait_us(40);  // most instructions take 40us
+  _e = 0;
+  wait_us(40);
+  _e = 1;
+  _d = value >> 0;
+  wait_us(40);
+  _e = 0;
+  wait_us(40);  // most instructions take 40us
+  _e = 1;
 }
 
 void TextLCD::writeCommand(int command) {
-    _rs = 0;
-    writeByte(command);
+  _rs = 0;
+  writeByte(command);
 }
 
 void TextLCD::writeData(int data) {
-    _rs = 1;
-    writeByte(data);
+  _rs = 1;
+  writeByte(data);
 }
 
 int TextLCD::address(int column, int row) {
@@ -113,48 +115,48 @@ int TextLCD::address(int column, int row) {
   MBED_ASSERT(column < columns());
   MBED_ASSERT(row >= 0);
   MBED_ASSERT(row < rows());
-    switch (_type) {
-      case LCDType::LCD20x4:
-            switch (row) {
-                case 0:
-                    return 0x80 + column;
-                case 1:
-                    return 0xc0 + column;
-                case 2:
-                    return 0x94 + column;
-                case 3:
+  switch (_type) {
+    case LCDType::LCD20x4:
+      switch (row) {
+        case 0:
+          return 0x80 + column;
+        case 1:
+          return 0xc0 + column;
+        case 2:
+          return 0x94 + column;
+        case 3:
         default:
-                    return 0xd4 + column;
-            }
-      case LCDType::LCD16x2B:
-            return 0x80 + (row * 40) + column;
-      case LCDType::LCD16x2:
-      case LCDType::LCD20x2:
-        default:
-            return 0x80 + (row * 0x40) + column;
-    }
+          return 0xd4 + column;
+      }
+    case LCDType::LCD16x2B:
+      return 0x80 + (row * 40) + column;
+    case LCDType::LCD16x2:
+    case LCDType::LCD20x2:
+    default:
+      return 0x80 + (row * 0x40) + column;
+  }
 }
 
 int TextLCD::columns() const {
-    switch (_type) {
-      case LCDType::LCD20x4:
-      case LCDType::LCD20x2:
-            return 20;
-      case LCDType::LCD16x2:
-      case LCDType::LCD16x2B:
-        default:
-            return 16;
-    }
+  switch (_type) {
+    case LCDType::LCD20x4:
+    case LCDType::LCD20x2:
+      return 20;
+    case LCDType::LCD16x2:
+    case LCDType::LCD16x2B:
+    default:
+      return 16;
+  }
 }
 
 int TextLCD::rows() const {
-    switch (_type) {
-      case LCDType::LCD20x4:
-            return 4;
-      case LCDType::LCD16x2:
-      case LCDType::LCD16x2B:
-      case LCDType::LCD20x2:
-        default:
-            return 2;
-    }
+  switch (_type) {
+    case LCDType::LCD20x4:
+      return 4;
+    case LCDType::LCD16x2:
+    case LCDType::LCD16x2B:
+    case LCDType::LCD20x2:
+    default:
+      return 2;
+  }
 }
--- TextLCD.h
+++ TextLCD.h
@@ -31,9 +31,9 @@
  * @code
  * #include "mbed.h"
  * #include "TextLCD.h"
- * 
+ *
  * TextLCD lcd(p10, p12, p15, p16, p29, p30); // rs, e, d4-d7
- * 
+ *
  * int main() {
  *     lcd.printf("Hello World!\n");
  * }
@@ -41,55 +41,57 @@
  */
 class TextLCD {
 public:
-    /** LCD panel format */
-    enum class LCDType {
-        LCD16x2     /**< 16x2 LCD panel (default) */
-        , LCD16x2B  /**< 16x2 LCD panel alternate addressing */
-        , LCD20x2   /**< 20x2 LCD panel */
-        , LCD20x4   /**< 20x4 LCD panel */
-    };
+  /** LCD panel format */
+  enum class LCDType {
+    LCD16x2,  /**< 16x2 LCD panel (default) */
+    LCD16x2B, /**< 16x2 LCD panel alternate addressing */
+    LCD20x2,  /**< 20x2 LCD panel */
+    LCD20x4   /**< 20x4 LCD panel */
+  };
 
-    /** Create a TextLCD interface
-     *
-     * @param rs    Instruction/data control line
-     * @param e     Enable line (clock)
-     * @param d4-d7 Data lines for using as a 4-bit interface
-     * @param type  Sets the panel size/addressing mode (default = LCD16x2)
-     */
-    TextLCD(PinName rs, PinName e, PinName d4, PinName d5, PinName d6, PinName d7, LCDType type = LCDType::LCD16x2);
+  /** Create a TextLCD interface
+   *
+   * @param rs    Instruction/data control line
+   * @param e     Enable line (clock)
+   * @param d4-d7 Data lines for using as a 4-bit interface
+   * @param type  Sets the panel size/addressing mode (default = LCD16x2)
+   */
+  TextLCD(
+      PinName rs, PinName e, PinName d4, PinName d5, PinName d6, PinName d7,
+      LCDType type = LCDType::LCD16x2);
 
-    /** Write a character to the LCD
-     *
-     * @param c The character to write to the display
-     */
-    int putc(int c);
+  /** Write a character to the LCD
+   *
+   * @param c The character to write to the display
+   */
+  int putc(int c);
 
-    /** Locate to a screen column and row
-     *
-     * @param column  The horizontal position from the left, indexed from 0
-     * @param row     The vertical position from the top, indexed from 0
-     */
-    void locate(int column, int row);
+  /** Locate to a screen column and row
+   *
+   * @param column  The horizontal position from the left, indexed from 0
+   * @param row     The vertical position from the top, indexed from 0
+   */
+  void locate(int column, int row);
 
-    /** Clear the screen and locate to 0,0 */
-    void cls();
+  /** Clear the screen and locate to 0,0 */
+  void cls();
 
-    int rows() const;
-    int columns() const;
+  int rows() const;
+  int columns() const;
 
 protected:
-    int _putc(int value);
+  int _putc(int value);
 
-    int address(int column, int row);
-    void character(int column, int row, int c);
-    void writeByte(int value);
-    void writeCommand(int command);
-    void writeData(int data);
+  int address(int column, int row);
+  void character(int column, int row, int c);
+  void writeByte(int value);
+  void writeCommand(int command);
+  void writeData(int data);
 
-    DigitalOut _rs, _e;
-    BusOut _d;
-    LCDType _type;
+  DigitalOut _rs, _e;
+  BusOut _d;
+  LCDType _type;
 
-    int _column;
-    int _row;
+  int _column;
+  int _row;
 };
